635f15f48d56f0ab51285f88d3cf7e073701dd0c
Drew Stephens <drew@dinomite.net>
2014-02-03 18:50:11 -0500

    Benchmarks for AtomicLong & LongAdder

---
+++ src/main/java/com/palominolabs/java7/AtomicLongBenchmark.java
@@ -0,0 +1,22 @@
+package com.palominolabs.java7;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AtomicLongBenchmark {
+    @State(Scope.Benchmark)
+    static class AtomicLongState {
+        public static final AtomicLong atomicLong = new AtomicLong();
+    }
+
+    @State(Scope.Thread)
+    public static class AtomicLongAction {
+        @GenerateMicroBenchmark
+        public void atomicLongBenchmark() {
+            AtomicLongState.atomicLong.getAndIncrement();
+        }
+    }
+}
---
+++ src/main/java/com/palominolabs/java8/LongAdderBenchmark.java
@@ -0,0 +1,22 @@
+package com.palominolabs.java8;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.concurrent.atomic.LongAdder;
+
+public class LongAdderBenchmark {
+    @State(Scope.Benchmark)
+    static class LongAdderState {
+        public static final LongAdder longAdder = new LongAdder();
+    }
+
+    @State(Scope.Thread)
+    public static class LongAdderAction {
+        @GenerateMicroBenchmark
+        public void longAdderBenchmark() {
+            LongAdderState.longAdder.increment();
+        }
+    }
+}
